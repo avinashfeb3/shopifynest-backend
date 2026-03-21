51ea7c5707d3d469c5d63de12602a6a23a905cb2
avinashfeb3 <avinash.webintern@gmail.com>
2026-03-21 11:02:13 +0530

    Refactor import statements for clarity and consistency

--- src/controllers/admin/products.controller.js
+++ src/controllers/admin/products.controller.js
@@ -1,10 +1,13 @@
-import { deleteImageFromCloudinary, uploadImageToCloudinary } from "../../helpers/cloudinary.js";
+import {
+  deleteImageFromCloudinary,
+  uploadImageToCloudinary,
+} from "../../helpers/cloudinary.js";
 import products from "../../models/products.model.js";
 
 // Get Products
 const getProducts = async (req, res) => {
   try {
-    const allProducts = await products.find().populate('category');
+    const allProducts = await products.find().populate("category");
     return res.status(200).json({
       success: true,
       message: "Products fetched successfully.",
@@ -67,34 +70,41 @@ const createProduct = async (req, res) => {
     } = req.body;
 
     if (
-      !name || name.trim() === "" ||
-      !category || category.trim() === "" ||
+      !name ||
+      name.trim() === "" ||
+      !category ||
+      category.trim() === "" ||
       !price ||
       !qty ||
-      !sku || sku.trim() === "" ||
-      !sizes || (typeof sizes === 'string' && sizes.trim() === "")
+      !sku ||
+      sku.trim() === "" ||
+      !sizes ||
+      (typeof sizes === "string" && sizes.trim() === "")
     ) {
       return res.status(400).json({
         success: false,
-        message:
-          "Name, category, price, qty, sku fields are required.",
+        message: "Name, category, price, qty, sku fields are required.",
         data: {},
       });
     }
 
     // sizes of the products
-    const sizesArray = sizes ? (typeof sizes === 'string' ? sizes.split(",") : sizes) : [];
+    const sizesArray = sizes
+      ? typeof sizes === "string"
+        ? sizes.split(",")
+        : sizes
+      : [];
 
     // handle image uploads
     const gallery = [];
-    if(req?.files){
-      for(const file of req?.files){
-        if(file?.path){
+    if (req?.files) {
+      for (const file of req?.files) {
+        if (file?.path) {
           const response = await uploadImageToCloudinary(file.path);
           gallery.push({
             url: response?.url,
             publicId: response?.public_id,
-          })
+          });
         }
       }
     }
@@ -110,9 +120,13 @@ const createProduct = async (req, res) => {
       price,
       discountPrice: discount_price,
       quantity: qty,
-      status: status ? status.charAt(0).toUpperCase() + status.slice(1) : "Inactive",
+      status: status
+        ? status.charAt(0).toUpperCase() + status.slice(1)
+        : "Inactive",
       qty,
-      isFeatured: is_featured ? is_featured.charAt(0).toUpperCase() + is_featured.slice(1) : "No",
+      isFeatured: is_featured
+        ? is_featured.charAt(0).toUpperCase() + is_featured.slice(1)
+        : "No",
       sizes: sizesArray,
       gallery,
     });
@@ -133,8 +147,7 @@ const createProduct = async (req, res) => {
 
 // Update Product
 const updateProduct = async (req, res) => {
-    try {
-      
+  try {
     // validate request
     const {
       name,
@@ -162,7 +175,11 @@ const updateProduct = async (req, res) => {
     }
 
     // sizes of the products
-    const sizesArray = sizes ? (typeof sizes === 'string' ? sizes.split(",") : sizes) : [];
+    const sizesArray = sizes
+      ? typeof sizes === "string"
+        ? sizes.split(",")
+        : sizes
+      : [];
 
     await products.updateOne(
       { _id: id },
@@ -177,11 +194,15 @@ const updateProduct = async (req, res) => {
           price,
           discountPrice: discount_price,
           quantity: qty,
-          status: status ? status.charAt(0).toUpperCase() + status.slice(1) : "Inactive",
-          isFeatured: is_featured ? is_featured.charAt(0).toUpperCase() + is_featured.slice(1) : "No",
+          status: status
+            ? status.charAt(0).toUpperCase() + status.slice(1)
+            : "Inactive",
+          isFeatured: is_featured
+            ? is_featured.charAt(0).toUpperCase() + is_featured.slice(1)
+            : "No",
           sizes: sizesArray,
-        }
-      }
+        },
+      },
     );
 
     // update product data in the database
@@ -204,40 +225,40 @@ const updateProduct = async (req, res) => {
 
 // Delete Product
 const deleteProduct = async (req, res) => {
-   try {
-     const { id } = req.params;
-     // check if the product exists
-     const product = await products.findById(id);
-     if (!product) {
-       return res.status(404).json({
-         success: false,
-         message: "Product not found.",
-         data: {},
-       });
-     }
+  try {
+    const { id } = req.params;
+    // check if the product exists
+    const product = await products.findById(id);
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found.",
+        data: {},
+      });
+    }
 
-     // first delete the images from cloudinary
-     if(product.gallery){
-       for(const image of product.gallery){
-         const public_id = image?.publicId;
-         await deleteImageFromCloudinary(public_id);
-       }
-     }
+    // first delete the images from cloudinary
+    if (product.gallery) {
+      for (const image of product.gallery) {
+        const public_id = image?.publicId;
+        await deleteImageFromCloudinary(public_id);
+      }
+    }
 
-     // delete the product from the database
-     await products.deleteOne({ _id: id });
+    // delete the product from the database
+    await products.deleteOne({ _id: id });
 
-     return res.status(200).json({
-       success: true,
-       message: "Product deleted successfully.",
-     });
-   } catch (error) {
-     return res.status(500).json({
-       success: false,
-       message: error.message || "Unable to delete product. Please try again later.",
-     });
-   }
-
+    return res.status(200).json({
+      success: true,
+      message: "Product deleted successfully.",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message:
+        error.message || "Unable to delete product. Please try again later.",
+    });
+  }
 };
 
 export { getProducts, getProduct, createProduct, deleteProduct, updateProduct };
